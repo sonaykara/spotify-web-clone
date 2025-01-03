4aea20b87c75e183b238e58f47df68618d205a88
Sonay Kara <zonaykara@gmail.com>
2025-01-04 00:06:22 +0300

    issue fiexed

--- spotify-clone/src/Layout/Layout.jsx
+++ spotify-clone/src/Layout/Layout.jsx
@@ -1,12 +1,9 @@
-import Footer from "../view/Footer"
-import Navbar from "../view/Navbar"
-import SideBar from "../view/SideBar"
+
 
 
 const Layout = () => {
   return (
     <div>
-      <Footer></Footer>
     </div>
   )
 }
