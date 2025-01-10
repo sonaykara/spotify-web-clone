42a47d74e93a13108357e9c565fd378d2984dc60
Sonay Kara <zonaykara@gmail.com>
2025-01-10 23:25:11 +0300

    fixed

--- spotify-clone/src/Layout/Layout.jsx
+++ spotify-clone/src/Layout/Layout.jsx
@@ -1,12 +1,8 @@
-import SearchPage from "../components/RouterPages/Search/SearchPage";
-import SearchPlayList from "../components/SearchPage/SearchPlaylist/SearchPlayList";
-import SearchTile from "../components/SearchPage/SearchTile/SearchTile";
+
 
 const Layout = () => {
   return (
     <div>
-    <SearchPage></SearchPage>
-    
   
     </div>
   );
